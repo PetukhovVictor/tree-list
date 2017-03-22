92e803d30d71526ce50d0c09c060bcd04614d27d
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-03-22 23:32:12 +0300

    Created application config.

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, 'out')
+    },
+    source: {
+        path: path.join(__dirname, 'src')
+    }
+};
