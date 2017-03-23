e99a77fc5b9cb3f092d72b5ff58113b26cf51129
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-03-23 19:11:57 +0300

    Created index.html page, changed output path.

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     output: {
-        path: path.join(__dirname, 'out')
+        path: path.join(__dirname, 'out/assets')
     },
     source: {
         path: path.join(__dirname, 'src')
