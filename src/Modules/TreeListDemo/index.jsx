31d312c281f6e5e0ea3fe6a8e625c3658fb9732d
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-03-24 11:25:49 +0300

    Created react class TreeListDemo; added scripts in package.json.

---
+++ src/Modules/TreeListDemo/index.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import {TreeListDemoServices} from './Services';
+
+const TreeListDemo = React.createClass({
+    displayName: 'TreeListDemo',
+
+    getInitialState () {
+        return {
+            isLoading: true
+        };
+    },
+
+    render () {
+
+    }
+});
---
+++ src/app.js
@@ -0,0 +1,1 @@
+import from 'Modules/TreeListDemo';
