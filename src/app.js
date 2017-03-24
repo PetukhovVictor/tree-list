804cb9f5ce54a9d728c4a9beab8a27b837c15a68
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-03-24 15:59:20 +0300

    Added creating of react element; simple layout.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const TreeListDemo = require('async-module-loader?name=TreeListDemo!Modules/TreeListDemo');
+import {TreeListDemo} from 'Modules/TreeListDemo';
 
-ReactDOM.render(TreeListDemo, document.getElementById('tree-list-container'));
+require('Styles/app.less');
+
+window.addEventListener('load', () => {
+    const TreeListDemoElement = React.createElement(TreeListDemo);
+
+    ReactDOM.render(
+        TreeListDemoElement,
+        document.getElementById('tree-list-container')
+    );
+});
