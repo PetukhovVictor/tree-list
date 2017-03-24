69423ba97523da6f050085a167c9beb23e57dcb0
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-03-24 15:52:42 +0300

    Added service for loading tree list from json.

--- src/Modules/TreeListDemo/Services/index.jsx
+++ src/Modules/TreeListDemo/Services/index.jsx
@@ -1,3 +1,12 @@
 export const TreeListDemoServices = {
-
+    loadFromJSON (path) {
+        return fetch(path).then(
+            response => {
+                return response.json();
+            },
+            () => {
+                throw 'Error tree list loading!';
+            }
+        )
+    }
 };
