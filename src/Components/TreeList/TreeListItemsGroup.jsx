377f6e3d212f85c035fa3822d77fbe3ba245b17e
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-03-26 20:41:39 +0300

    Created highlight and mark active item functional.

--- src/Components/TreeList/TreeListItemsGroup.jsx
+++ src/Components/TreeList/TreeListItemsGroup.jsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { findDOMNode } from 'react-dom';
-import {isArray, isFunction} from 'lodash';
+import {isArray, isFunction, isUndefined} from 'lodash';
 import classNames from 'classnames';
 import $ from 'jquery';
+
+import {PersistState} from 'Utils/PersistState';
 
 require('Styles/Components/TreeList.less');
 
@@ -11,8 +13,13 @@ export const TreeListItemsGroup = React.createClass({
         expandable: React.PropTypes.bool,
         expanded: React.PropTypes.bool,
         classes: React.PropTypes.array,
-        item: React.PropTypes.object.isRequired
+        item: React.PropTypes.object.isRequired,
+        highlightRule: React.PropTypes.object,
+        activeItem: React.PropTypes.string
     },
+
+    persistStateKeyPrefix: 'navigation_item_',
+    persistStateItems: ['expanded'],
 
     getDefaultProps: function() {
         return {
@@ -28,17 +35,56 @@ export const TreeListItemsGroup = React.createClass({
         };
     },
 
+    componentDidMount () {
+        const {item:{id}} = this.props;
+
+        PersistState.get(`${this.persistStateKeyPrefix}${id}`).then((persistState) => {
+            !isUndefined(persistState) && this.setState(persistState);
+        });
+    },
+
+    componentDidUpdate() {
+        const {item:{id}} = this.props;
+        const state = this.state;
+        const persistState = {};
+
+        this.persistStateItems.forEach((item) => {
+            if (!isUndefined(state[item])) {
+                persistState[item] = state[item];
+            }
+        });
+
+        PersistState.set(`${this.persistStateKeyPrefix}${id}`, persistState);
+    },
+
     hasChildren () {
         const pages = this.props.item.pages;
 
         return isArray(pages) && pages.length !== 0;
     },
 
+    highlight (item, rule) {
+        if (rule.isStrict && item[rule.field] == rule.value) {
+            return <b>{item.title}</b>;
+        } else if (!rule.isStrict && rule.field == 'title') {
+            return item.title.split(new RegExp(`(${rule.value})`, 'i'))
+                .map((textElement, index) => index % 2 == 0 ? textElement : <b key={`item-title-bold-${index}`}>{textElement}</b>);
+        } else {
+            return item.title;
+        }
+    },
+
+    markActiveItem(item, activeItem) {
+        return item.id == activeItem ? <b>{item.title}</b> : null;
+    },
+
     handleItemClick (e) {
         e.preventDefault();
         let {expanded} = this.state;
-        const slideToggle = (callback) => {
+        const slideToggle = (callback, preHide) => {
+            preHide = preHide || false;
             const el = findDOMNode(this.refs['children-pages']);
+            preHide && $(el).hide();
             $(el).slideToggle(() => isFunction(callback) && callback());
         };
         const setState = (callback) => {
@@ -52,16 +98,22 @@ export const TreeListItemsGroup = React.createClass({
             this.setState({ isExpandingDown: true });
             slideToggle(setState);
         } else {
-            setState(slideToggle);
+            setState(() => slideToggle(null, true));
         }
     },
 
     renderItems (pages) {
+        const {highlightRule, activeItem} = this.props;
         let pageElements = [];
 
         pages.forEach((page, index) => {
             pageElements.push(
-                <TreeListItemsGroup item={page} />
+                <TreeListItemsGroup
+                    key={page.id}
+                    item={page}
+                    highlightRule={highlightRule}
+                    activeItem={activeItem}
+                />
             );
         });
 
@@ -69,7 +121,7 @@ export const TreeListItemsGroup = React.createClass({
     },
 
     renderTitle () {
-        const {expandable, item} = this.props;
+        const {expandable, item, highlightRule, activeItem} = this.props;
         const hasChildren = this.hasChildren();
         let titleElement;
 
@@ -78,8 +130,15 @@ export const TreeListItemsGroup = React.createClass({
                 <span className="page-item-title">{item.title}</span>
             );
         } else {
+            let itemTitle = item.title;
+            if (highlightRule !== null) {
+                itemTitle = this.highlight(item, highlightRule);
+            }
+            if (activeItem !== null) {
+                itemTitle = this.markActiveItem(item, activeItem) || itemTitle;
+            }
             titleElement = (
-                <a href="#" onClick={this.handleItemClick} className="page-item-title">{item.title}</a>
+                <a href="#" onClick={this.handleItemClick} className="page-item-title">{itemTitle}</a>
             );
         }
 
@@ -93,7 +152,7 @@ export const TreeListItemsGroup = React.createClass({
         const classes = {
             'page-item': true,
             'expandable': expandable && hasChildren,
-            'expanded': expanded && !isExpandingDown,
+            'expanded': expanded && !isExpandingDown
         };
 
         return (
